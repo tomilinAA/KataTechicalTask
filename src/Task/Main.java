ba05b1469e3c87f407e95ad6f6ddf6c6c8bb5578
USER <tomilin-2000@mail.ru>
2022-05-06 12:29:12 +0300

    TEST2

--- src/Task/Main.java
+++ src/Task/Main.java
@@ -1,13 +1,12 @@
 package Task;
 
-import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
     static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) throws Exception {
-        System.out.println("ВВедем ��� ��������. ������ 2 + 2 ��� I + I");
+        System.out.println("Введи мат. операцию. Пример: 2 + 2 или I + I");
         String res = scanner.nextLine();
         System.out.println(calc(res));
     }
@@ -15,13 +14,11 @@ public class Main {
     public static String calc(String input) throws Exception {
         String[] arr = input.split(" ");
         if (arr.length > 3) {
-            throw new Exception("�.�. ������ �������������� �������� �� ������������� ������� - ��� �������� � ���� �������� (+, -, /, *)");
+            throw new Exception("т.к. формат математической операции не удовлетворяет заданию - два операнда и один оператор (+, -, /, *)");
         } else if (arr.length == 1) {
-            throw new Exception("�.�. ������ �� �������� �������������� ���������");
-        } else if (arr[1].equals(" ")) {
-            throw new Exception("�������� ������ �����. 1 + 1");
+            throw new Exception("т.к. строка не является математической операцией");
         }
-        int res = 0;
+        int res;
         boolean Roman = false;
         Roman roman = new Roman();
         if (roman.testRomanNum(arr[0]) && roman.testRomanNum(arr[2])) {
@@ -35,33 +32,24 @@ public class Main {
             num1 = Integer.parseInt(arr[0]);
             num2 = Integer.parseInt(arr[2]);
         } catch (Exception exception) {
-            throw new Exception("�.�. ������������ ������������ ������ ������� ���������");
+            throw new Exception("т.к. используются одновремено разные системы счисления");
         }
 
             if ((num1 < 0 || num1 > 10) || (num2 < 0 || num2 > 10)) {
-                throw new Exception("�� ���� ����� �� 1 �� 10 ������������, �� �����");
+                throw new Exception("Калькулятор принимает на вход числа от 1 до 10 включительно, не более");
             }
             {
-                switch (arr[1]) {
-                    case "+":
-                        res = num1 + num2;
-                        break;
-                    case "-":
-                        res = num1 - num2;
-                        break;
-                    case "*":
-                        res = num1 * num2;
-                        break;
-                    case "/":
-                        res = num1 / num2;
-                        break;
-                    default:
-                        throw new Exception("������ ������������ �������. (+,-,*,/)");
-                }
+                res = switch (arr[1]) {
+                    case "+" -> num1 + num2;
+                    case "-" -> num1 - num2;
+                    case "*" -> num1 * num2;
+                    case "/" -> num1 / num2;
+                    default -> throw new Exception("т.к. необходимо ввести подходящий операнд . (+,-,*,/)");
+                };
 
                 if (Roman) {
                     if (res <= 0) {
-                        throw new Exception("�.�. � ������� ������� ��� ������������� �����");
+                        throw new Exception("т.к. в римской системе нет отрицательных чисел");
                     }
                     return roman.romanValue[res];
                 }
