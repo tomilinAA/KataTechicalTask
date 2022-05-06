e8cd64337f3691b337e6eff77b1975798ee81c07
USER <tomilin-2000@mail.ru>
2022-05-06 11:44:46 +0300

    First commit

---
+++ src/Task/Main.java
@@ -0,0 +1,81 @@
+package Task;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("Введите мат операцию. Пример 2 + 2 или I + I");
+        String res = scanner.nextLine();
+        System.out.println(calc(res));
+    }
+
+    public static String calc(String input) throws Exception {
+        String[] arr = input.split(" ");
+        if (arr.length > 3) {
+            throw new Exception("т.к. формат математической операции не удовлетворяет заданию - два операнда и один оператор (+, -, /, *)");
+        } else if (arr.length == 1) {
+            throw new Exception("т.к. строка не является математической операцией");
+        } else if (arr[1].equals(" ")) {
+            throw new Exception("неверный формат ввода. 1 + 1");
+        }
+        int res = 0;
+        boolean Roman = false;
+        Roman roman = new Roman();
+        if (roman.testRomanNum(arr[0]) && roman.testRomanNum(arr[2])) {
+            arr[0] = roman.NumbersOfRoman(arr[0]);
+            arr[2] = roman.NumbersOfRoman(arr[2]);
+            Roman = true;
+        }
+            int num1;
+        int num2;
+        try {
+            num1 = Integer.parseInt(arr[0]);
+            num2 = Integer.parseInt(arr[2]);
+        } catch (Exception exception) {
+            throw new Exception("т.к. используются одновременно разные системы счисления");
+        }
+
+            if ((num1 < 0 || num1 > 10) || (num2 < 0 || num2 > 10)) {
+                throw new Exception("на вход числа от 1 до 10 включительно, не более");
+            }
+            {
+                switch (arr[1]) {
+                    case "+":
+                        res = num1 + num2;
+                        break;
+                    case "-":
+                        res = num1 - num2;
+                        break;
+                    case "*":
+                        res = num1 * num2;
+                        break;
+                    case "/":
+                        res = num1 / num2;
+                        break;
+                    default:
+                        throw new Exception("Введен недопустимый операнд. (+,-,*,/)");
+                }
+
+                if (Roman) {
+                    if (res <= 0) {
+                        throw new Exception("т.к. в римской системе нет отрицательных чисел");
+                    }
+                    return roman.romanValue[res];
+                }
+                return String.valueOf(res);
+            }
+        }
+    }
+
+
+
+
+
+
+
+
+
+
---
+++ src/Task/Roman.java
@@ -0,0 +1,31 @@
+package Task;
+
+public class Roman {
+    String[] romanValue = {"null", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X",
+            "XI", "XII", "XIII", "XIV", "XV", "XVI", "XVII", "XVIII", "XIX", "XX",
+            "XXI", "XXII", "XXIII", "XXIV", "XXV", "XXVI", "XXVII", "XXVIII", "XXIX", "XXX",
+            "XXXI", "XXXII", "XXXIII", "XXXIV", "XXXV", "XXXVI", "XXXVII", "XXXVIII", "XXXIX", "XL",
+            "XLI", "XLII", "XLIII", "XLIV", "XLV", "XLVI", "XLVII", "XLVIII", "XLIX", "L",
+            "LI", "LII", "LIII", "LIV", "LV", "LVI", "LVII", "LVIII", "LIX", "LX",
+            "LXI", "LXII", "LXIII", "LXIV", "LXV", "LXVI", "LXVII", "LXVIII", "LXIX", "LXX",
+            "LXXI", "LXXII", "LXXIII", "LXXIV", "LXXV", "LXXVI", "LXXVII", "LXXVIII", "LXXIX", "LXXX",
+            "LXXXI", "LXXXII", "LXXXIII", "LXXXIV", "LXXXV", "LXXXVI", "LXXXVII", "LXXXVIII", "LXXXIX", "XC",
+            "XCI", "XCII", "XCIII", "XCIV", "XCV", "XCVI", "XCVII", "XCVIII", "XCIX", "C"};
+
+   public boolean testRomanNum(String input) {
+        for (int i = 0; i < romanValue.length; i++) {
+            if(input.equals(romanValue[i])) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public String NumbersOfRoman(String str) {
+        for (int i = 1; i < romanValue.length; i++) {
+            if (romanValue[i].equals(str))
+                return String.valueOf(i);
+        }
+        return "";
+    }
+}
